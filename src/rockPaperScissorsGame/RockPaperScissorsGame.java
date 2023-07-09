40e7f939949dc077527ada8c651cd5be562183a4
Melissa Astbury <melissa.astbury@footasylum.com>
2023-07-09 19:59:52 +0200

    fixed calculation of points

--- src/rockPaperScissorsGame/RockPaperScissorsGame.java
+++ src/rockPaperScissorsGame/RockPaperScissorsGame.java
@@ -10,13 +10,15 @@ public class RockPaperScissorsGame {
 		// Initialise variables
 		int roundsToPlay = 0;
 		String output = "";
-		int minValue = 0;
-		int maxValue = 0;
 		int playerOneOption = -1;
 		int playerTwoOption = -1;
 		
 		int playerOnePoints = 0;
 		int playerTwoPoints = 0;
+		
+		int rock = 0;
+		int paper = 1;
+		int scissors = 2;
 		
 		// Method called to get a value for the number of rounds the users wish to play
 		roundsToPlay = ChosenNumberOfRounds();
@@ -29,25 +31,32 @@ public class RockPaperScissorsGame {
 			// Player must choose rock, paper or scissors which corresponds to a number. This number must be 0, 1 or 2. If not the user will be asked again for another number.
 			while (playerOneOption == -1) {
 				String playerOneInput = JOptionPane.showInputDialog("Player 1, choose one of the following: \n Rock: 0 \n Paper: 1 \n Scissors: 2");
-				if(IsIntegerInRange(playerOneInput, minValue, maxValue)) {
+				if(IsIntegerInRange(playerOneInput, 0, 2)) {
 					playerOneOption = Integer.parseInt(playerOneInput);
 				};
 			}
 			while(playerTwoOption == -1) {
 				String playerTwoInput = JOptionPane.showInputDialog("Player 2, choose one of the following: \n Rock: 0 \n Paper: 1 \n Scissors: 2");
-				if(IsIntegerInRange(playerTwoInput, minValue, maxValue)) {
+				if(IsIntegerInRange(playerTwoInput, 0, 2)) {
 					playerTwoOption = Integer.parseInt(playerTwoInput);
 				};
 			}
 	
 			// Calculate the points each player is to get depending on their chosen option
-			CalculatePoints(playerOneOption, playerTwoOption, playerOnePoints, playerTwoPoints);
+			// Only put a condition in for the times you are to win against component
+			if (playerOneOption == rock && playerTwoOption == scissors || playerOneOption == paper && playerTwoOption == rock || playerOneOption == scissors && playerTwoOption == paper){
+				// Give a point to player one
+				playerOnePoints = playerOnePoints + 1;
+			} else if (playerTwoOption == rock && playerOneOption == scissors || playerTwoOption == paper && playerOneOption == rock || playerTwoOption == scissors && playerOneOption == paper){
+				// Give a point to player two
+				playerTwoPoints = playerTwoPoints + 1;
+			}
 			// After each calculation the options need to be reset to allow the users to choose another option to play with in the next round
 			playerOneOption = -1;
 			playerTwoOption = -1;
 			
 			// Construct output to be displayed back to the user
-			output = "Rounds played: " + roundsToPlay + " PlayerOnePoints: " + playerOnePoints + " and PlayerTwoPoints " + playerTwoPoints;
+			output = "PlayerOnePoints: " + playerOnePoints + " and PlayerTwoPoints " + playerTwoPoints;
 			// Display the message showing how many rounds the user has chosen to play
 			JOptionPane.showMessageDialog(null, output, "", JOptionPane.INFORMATION_MESSAGE);
 		}
@@ -69,31 +78,14 @@ public class RockPaperScissorsGame {
 		catch (Exception e) {
 			return false;
 		}
-	}
-	
-	// Calculate points
-	public static void CalculatePoints(int playerOneOption, int playerTwoOption, int playerOnePoints, int playerTwoPoints)
-	{
-		int rock = 0;
-		int paper = 1;
-		int scissors = 2;
-		
-		if (playerOneOption == rock && playerTwoOption == scissors || playerOneOption == paper && playerTwoOption == rock || playerOneOption == scissors && playerTwoOption == paper){
-			// Give a point to player one
-			playerOnePoints = playerOnePoints + 1;
-		} else if (playerTwoOption == rock && playerOneOption == scissors || playerTwoOption == paper && playerOneOption == rock || playerTwoOption == scissors && playerOneOption == paper){
-			// Give a point to player two
-			playerTwoPoints = playerTwoPoints + 1;
-		}
-	}
-	
+	}	
 	
 	// NOTES:
 	// 1. Ask users how many rounds they wish to play - DONE
 	// 2. Ask player one for a number - DONE
 	// 3. Ask player two for a number - DONE
 	// 4. Add validation to step 2 & 3 - DONE
-	// 5. Calculate the rounds for points - NEEDS FIXING, DOES NOT WORK
+	// 5. Calculate the rounds for points - DONE
 	// 6. Display the winner of each round
 	// 7. Announce the winner of the total rounds and ask if they wish to play again
 }
