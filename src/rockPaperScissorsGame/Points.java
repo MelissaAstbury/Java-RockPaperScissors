1d9a10d6f14545fa02a50adb114fefc4a9f94165
Melissa Astbury <melissa.astbury@footasylum.com>
2023-07-12 11:32:17 +0200

    ability to play multiple games and calculate the winner

---
+++ src/rockPaperScissorsGame/Points.java
@@ -0,0 +1,39 @@
+package rockPaperScissorsGame;
+
+import javax.swing.JOptionPane;
+
+public class Points {
+	int playerOne;
+	int playerTwo;
+
+	public Points CalculatePoints(int playerOneOption, int playerTwoOption) {
+		String output = "";
+		int rock = 0;
+		int paper = 1;
+		int scissors = 2;
+		
+		Points playersPoints = new Points();
+		
+		// Calculate the points each player is to get depending on their chosen option
+		// Only put a condition in for the times you are to win against component
+		if (playerOneOption == rock && playerTwoOption == scissors || playerOneOption == paper && playerTwoOption == rock || playerOneOption == scissors && playerTwoOption == paper){
+			playersPoints.playerOne = this.playerOne + 1;
+			//Construct output to be displayed back to the user
+			output = "Player 1 won this round!";
+			// Display the message showing how many rounds the user has chosen to play
+			DisplayMessage(output);
+		} else if (playerTwoOption == rock && playerOneOption == scissors || playerTwoOption == paper && playerOneOption == rock || playerTwoOption == scissors && playerOneOption == paper){
+			playersPoints.playerTwo = this.playerTwo  + 1;
+			//Construct output to be displayed back to the user
+			output = "Player 2 won this round!";
+			// Display the message showing how many rounds the user has chosen to play
+			DisplayMessage(output);
+		}
+		
+		return playersPoints;
+	}
+	
+	private static void DisplayMessage(String output) {
+		JOptionPane.showMessageDialog(null, output, "", JOptionPane.INFORMATION_MESSAGE);
+	}
+}
--- src/rockPaperScissorsGame/RockPaperScissorsGame.java
+++ src/rockPaperScissorsGame/RockPaperScissorsGame.java
@@ -5,26 +5,24 @@ import javax.swing.JOptionPane;
 public class RockPaperScissorsGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		// Initialise variables
 		int roundsToPlay = 0;
 		String output = "";
 		int playerOneOption = -1;
 		int playerTwoOption = -1;
-		
 		int playerOnePoints = 0;
 		int playerTwoPoints = 0;
+		Boolean playAgain = true;
 		
-		int rock = 0;
-		int paper = 1;
-		int scissors = 2;
+		Points playersPoints = new Points();
 		
+		while (playAgain) {
+			
 		// Method called to get a value for the number of rounds the users wish to play
 		roundsToPlay = ChosenNumberOfRounds();
 		// Construct and display message to users to display the amount of rounds they have chosen 
 		output = "You have chosen to play " + roundsToPlay + " games. Let the games begin!";
-		JOptionPane.showMessageDialog(null, output, "", JOptionPane.INFORMATION_MESSAGE);
+		DisplayMessage(output);
 		
 		// Loop through each round so the users can choose different options (rock, paper or scissors) on each round
 		for(int i = 0; i < roundsToPlay; i++) {
@@ -41,27 +39,31 @@ public class RockPaperScissorsGame {
 					playerTwoOption = Integer.parseInt(playerTwoInput);
 				};
 			}
-	
-			// Calculate the points each player is to get depending on their chosen option
-			// Only put a condition in for the times you are to win against component
-			if (playerOneOption == rock && playerTwoOption == scissors || playerOneOption == paper && playerTwoOption == rock || playerOneOption == scissors && playerTwoOption == paper){
-				// Give a point to player one
-				playerOnePoints = playerOnePoints + 1;
-			} else if (playerTwoOption == rock && playerOneOption == scissors || playerTwoOption == paper && playerOneOption == rock || playerTwoOption == scissors && playerOneOption == paper){
-				// Give a point to player two
-				playerTwoPoints = playerTwoPoints + 1;
-			}
+			
+			playersPoints = playersPoints.CalculatePoints(playerOneOption, playerTwoOption);
+			// Store result in local variable
+			playerOnePoints += playersPoints.playerOne;
+			playerTwoPoints += playersPoints.playerTwo;
 			// After each calculation the options need to be reset to allow the users to choose another option to play with in the next round
 			playerOneOption = -1;
 			playerTwoOption = -1;
+		}
+		
+			AnnounceWinner(playerOnePoints, playerTwoPoints);
 			
-			// Construct output to be displayed back to the user
-			output = "PlayerOnePoints: " + playerOnePoints + " and PlayerTwoPoints " + playerTwoPoints;
-			// Display the message showing how many rounds the user has chosen to play
-			JOptionPane.showMessageDialog(null, output, "", JOptionPane.INFORMATION_MESSAGE);
+			int result = JOptionPane.showConfirmDialog(null, "Do you wish to play again?", "Confirmation", JOptionPane.YES_NO_OPTION);
+			if (result == JOptionPane.NO_OPTION) {
+				playAgain = false;
+			}
 		}
 	}
 	
+	// Generic message functionality
+	public static void DisplayMessage(String output) {
+		JOptionPane.showMessageDialog(null, output, "", JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+	// PLayers to choose how many rounds they wish to play
 	public static int ChosenNumberOfRounds() {
 		// Prompt user to state how many rounds they wish to play
 		String userInput = JOptionPane.showInputDialog("Welcome to Rock, Paper, Scissors! \n How many rounds would you wish to play?");
@@ -80,12 +82,37 @@ public class RockPaperScissorsGame {
 		}
 	}	
 	
+	// Check which player has the most points and reveal the winner of the game after all rounds have been played
+	public static void AnnounceWinner(int playerOnePoints, int playerTwoPoints) {
+		String winnerOutput = "";
+		
+		if (playerOnePoints > playerTwoPoints) {
+			// Construct output to be displayed back to the user
+			winnerOutput = "Player 1 has won the game!";
+			// Display the message showing how many rounds the user has chosen to play
+			JOptionPane.showMessageDialog(null, winnerOutput, "", JOptionPane.INFORMATION_MESSAGE);
+		} else if (playerTwoPoints > playerOnePoints) {
+			// Construct output to be displayed back to the user
+			winnerOutput = "Player 2 has won the game!";
+			// Display the message showing how many rounds the user has chosen to play
+			JOptionPane.showMessageDialog(null, winnerOutput, "", JOptionPane.INFORMATION_MESSAGE);
+		} else {
+			// Construct output to be displayed back to the user
+			winnerOutput = "This game was a draw!";
+			// Display the message showing how many rounds the user has chosen to play
+			JOptionPane.showMessageDialog(null, winnerOutput, "", JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
+	
+	
+	
 	// NOTES:
 	// 1. Ask users how many rounds they wish to play - DONE
 	// 2. Ask player one for a number - DONE
 	// 3. Ask player two for a number - DONE
 	// 4. Add validation to step 2 & 3 - DONE
 	// 5. Calculate the rounds for points - DONE
-	// 6. Display the winner of each round
-	// 7. Announce the winner of the total rounds and ask if they wish to play again
+	// 6. Display the winner of each round - DONE
+	// 7. Announce the winner of the total rounds - DONE
+	// 8. Ask if they wish to play again - DONE
 }
